f6f79c23de6460719efab6e44ffb5cc3482f1a34
Dan Cryer <dan@block8.co.uk>
2015-08-27 16:59:46 +0100

    Migrations

---
+++ Migration/20150827144921_file_install_migration.php
@@ -0,0 +1,76 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class FileInstallMigration extends AbstractMigration
+{
+    public function up()
+    {
+        // Create tables:
+        $this->createFile();
+
+        // Add foreign keys:
+        $table = $this->table('file');
+
+        if (!$table->hasForeignKey('user_id')) {
+            $table->addForeignKey('user_id', 'user', 'id', ['delete' => 'SET_NULL', 'update' => 'CASCADE']);
+            $table->save();
+        }
+    }
+
+    protected function createFile()
+    {
+        $table = $this->table('file', ['id' => false, 'primary_key' => ['id']]);
+
+        if (!$this->hasTable('file')) {
+            $table->addColumn('id', 'char', ['limit' => 32, 'null' => false]);
+            $table->create();
+        }
+
+        if (!$table->hasColumn('scope')) {
+            $table->addColumn('scope', 'string', ['limit' => 50, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('filename')) {
+            $table->addColumn('filename', 'string', ['limit' => 250, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('title')) {
+            $table->addColumn('title', 'string', ['limit' => 250, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('mime_type')) {
+            $table->addColumn('mime_type', 'string', ['limit' => 50, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('extension')) {
+            $table->addColumn('extension', 'string', ['limit' => 10, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('created_date')) {
+            $table->addColumn('created_date', 'datetime', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('updated_date')) {
+            $table->addColumn('updated_date', 'datetime', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('user_id')) {
+            $table->addColumn('user_id', 'integer', ['signed' => false, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('size')) {
+            $table->addColumn('size', 'integer', ['signed' => false, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('meta')) {
+            $table->addColumn('meta', 'text', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasIndex('user_id')) {
+            $table->addIndex('user_id', ['unique' => false]);
+        }
+
+        $table->save();
+    }
+}
